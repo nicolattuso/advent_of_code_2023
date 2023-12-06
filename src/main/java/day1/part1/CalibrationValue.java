ac6b4841da835e240f69ecfb43db784c17b0c19e
Nicolas Jean <nicojean23@gmail.com>
2023-12-06 15:15:58 +0100

    Implement day 1 part 1 example

---
+++ src/main/java/day1/part1/CalibrationValue.java
@@ -0,0 +1,3 @@
+package day1.part1;
+
+public record CalibrationValue (int value) {}
--- src/main/java/day1/part1/Solver.java
+++ src/main/java/day1/part1/Solver.java
@@ -1,7 +1,15 @@
 package day1.part1;
 
 public class Solver {
+  public static CalibrationValue parseLine(String line) {
+    var firstDigit = line.chars().filter(Character::isDigit).findFirst().getAsInt();
+    var lastDigit = new StringBuilder(line).reverse().toString().chars().filter(Character::isDigit).findFirst().getAsInt();
+    String calibrationValueString = String.valueOf((char) firstDigit) +
+        (char) lastDigit;
+    return new CalibrationValue(Integer.parseInt(calibrationValueString));
+  }
+
   public static int solve(String input) {
-    return 142;
+    return input.lines().map(Solver::parseLine).mapToInt(CalibrationValue::value).sum();
   }
 }
